b9ed70cbf585299f8337863b03cbdaa018736aa2
airihiga <rainboop118@gmail.com>
2019-06-13 20:37:23 +0900

    commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Folder;
+
+use App\Task;
+
+class TaskController extends Controller
+{
+    public function index(int $id)
+{
+    // すべてのフォルダを取得する
+    $folders = Folder::all();
+
+    // 選ばれたフォルダを取得する
+    $current_folder = Folder::find($id);
+
+    // 選ばれたフォルダに紐づくタスクを取得する
+    $tasks = Task::where('folder_id', $current_folder->id)->get();
+
+    return view('tasks/index', [
+        'folders' => $folders,
+        'current_folder_id' => $current_folder->id,
+        'tasks' => $tasks,
+    ]);
+}
+}
---
+++ app/Task.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    const STATUS = [
+        1 => [ 'label' => '未着手' ],
+        2 => [ 'label' => '着手中' ],
+        3 => [ 'label' => '完了' ],
+    ];
+
+    public function getStatusLabelAttribute()
+    {
+        $status = $this->attributes['status'];
+
+        if(!isset(self::STATUS[$status]))
+    }
+
+}
